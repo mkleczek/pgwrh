c2f43cdd6378eaae8d0fdffa76ff3e5dbfefdba0
Michal Kleczek <michal.kleczek@sap.com>
2024-10-22 11:25:41 +0200

    Updated test.sql to create more realistic configuration

--- test.sql
+++ test.sql
@@ -1,49 +1,48 @@
 CREATE ROLE test_replica;
 
 CREATE SCHEMA IF NOT EXISTS test;
+CREATE SCHEMA IF NOT EXISTS test_shards;
 
 GRANT USAGE ON SCHEMA test TO test_replica;
 ALTER DEFAULT PRIVILEGES IN SCHEMA test GRANT SELECT ON TABLES TO test_replica;
 
-CREATE TABLE test.parent (col1 text, col2 text) PARTITION BY HASH (col2);
-CREATE TABLE test.child0 PARTITION OF parent (PRIMARY KEY (col1)) FOR VALUES WITH (MODULUS 16, REMAINDER 0);
-CREATE TABLE test.child1 PARTITION OF parent (PRIMARY KEY (col1)) FOR VALUES WITH (MODULUS 16, REMAINDER 1);
-CREATE TABLE test.child2 PARTITION OF parent (PRIMARY KEY (col1)) FOR VALUES WITH (MODULUS 16, REMAINDER 2);
-CREATE TABLE test.child3 PARTITION OF parent (PRIMARY KEY (col1)) FOR VALUES WITH (MODULUS 16, REMAINDER 3);
-CREATE TABLE test.child4 PARTITION OF parent (PRIMARY KEY (col1)) FOR VALUES WITH (MODULUS 16, REMAINDER 4);
-CREATE TABLE test.child5 PARTITION OF parent (PRIMARY KEY (col1)) FOR VALUES WITH (MODULUS 16, REMAINDER 5);
-CREATE TABLE test.child6 PARTITION OF parent (PRIMARY KEY (col1)) FOR VALUES WITH (MODULUS 16, REMAINDER 6);
-CREATE TABLE test.child7 PARTITION OF parent (PRIMARY KEY (col1)) FOR VALUES WITH (MODULUS 16, REMAINDER 7);
-CREATE TABLE test.child8 PARTITION OF parent (PRIMARY KEY (col1)) FOR VALUES WITH (MODULUS 16, REMAINDER 8);
-CREATE TABLE test.child9 PARTITION OF parent (PRIMARY KEY (col1)) FOR VALUES WITH (MODULUS 16, REMAINDER 9);
-CREATE TABLE test.child10 PARTITION OF parent (PRIMARY KEY (col1)) FOR VALUES WITH (MODULUS 16, REMAINDER 10);
-CREATE TABLE test.child11 PARTITION OF parent (PRIMARY KEY (col1)) FOR VALUES WITH (MODULUS 16, REMAINDER 11);
-CREATE TABLE test.child12 PARTITION OF parent (PRIMARY KEY (col1)) FOR VALUES WITH (MODULUS 16, REMAINDER 12);
-CREATE TABLE test.child13 PARTITION OF parent (PRIMARY KEY (col1)) FOR VALUES WITH (MODULUS 16, REMAINDER 13);
-CREATE TABLE test.child14 PARTITION OF parent (PRIMARY KEY (col1)) FOR VALUES WITH (MODULUS 16, REMAINDER 14);
-CREATE TABLE test.child15 PARTITION OF parent (PRIMARY KEY (col1)) FOR VALUES WITH (MODULUS 16, REMAINDER 15);
+CREATE TABLE test.my_data (col1 text, col2 text, col3 date) PARTITION BY RANGE (col3);
 
-CREATE USER h1 PASSWORD 'h1' REPLICATION IN ROLE test_replica;
-CREATE USER h2 PASSWORD 'h2' REPLICATION IN ROLE test_replica;
+CREATE TABLE test.my_data_2025 PARTITION OF test.my_data FOR VALUES FROM (make_date(2025, 1, 1)) TO (make_date(2026, 1, 1)) PARTITION BY HASH (col2);
+CREATE TABLE test.my_data_2024 PARTITION OF test.my_data FOR VALUES FROM (make_date(2024, 1, 1)) TO (make_date(2025, 1, 1)) PARTITION BY HASH (col2);
+CREATE TABLE test.my_data_2023 PARTITION OF test.my_data FOR VALUES FROM (make_date(2023, 1, 1)) TO (make_date(2024, 1, 1))  PARTITION BY HASH (col2);
+CREATE TABLE test.my_data_2022 PARTITION OF test.my_data FOR VALUES FROM (make_date(2022, 1, 1)) TO (make_date(2023, 1, 1))  PARTITION BY HASH (col2);
 
-INSERT INTO pgwrh.replication_group (replication_group_id, username, password) VALUES ('g1', 'u', 'p');
-INSERT INTO pgwrh.sharded_table (replication_group_id, sharded_table_schema, sharded_table_name, replica_count)
-VALUES ('g1', 'test', 'parent', 2);
-
-SELECT add_shard_host('g1', 'h1', 'localhost', 5533);
-SELECT add_shard_host('g1', 'h2', 'localhost', 5534);
-SELECT add_shard_host('g1', 'h3', 'localhost', 5535);
-SELECT add_shard_host('g1', 'h4', 'localhost', 5536);
-
-DO$$
+DO
+$$
 DECLARE
     r record;
 BEGIN
     FOR r IN
         SELECT
-            format('CREATE TABLE test.my_data_%1$s_%2$s PARTITION OF test.my_data_%1$s (PRIMARY KEY (col1)) FOR VALUES WITH (MODULUS 16, REMAINDER %2$s)', year, rem) stmt
-        FROM generate_series(2022, 2023) year, generate_series(0, 15) rem
+            format('CREATE TABLE test_shards.my_data_%1$s_%2$s PARTITION OF test.my_data_%1$s (PRIMARY KEY (col1)) FOR VALUES WITH (MODULUS 16, REMAINDER %2$s)', year, rem) stmt
+        FROM generate_series(2022, 2025) year, generate_series(0, 15) rem
     LOOP
         EXECUTE r.stmt;
     END LOOP;
-END$$;
+END
+$$;
+
+INSERT INTO pgwrh.replication_group
+        (replication_group_id, username, password)
+    VALUES
+        ('g1', 'u', 'p');
+INSERT INTO pgwrh.sharded_table
+        (replication_group_id, sharded_table_schema, sharded_table_name, replica_count)
+    VALUES
+        ('g1','test', 'my_data', 2);
+
+CREATE USER h1 PASSWORD 'h1' REPLICATION IN ROLE test_replica;
+CREATE USER h2 PASSWORD 'h2' REPLICATION IN ROLE test_replica;
+CREATE USER h3 PASSWORD 'h3' REPLICATION IN ROLE test_replica;
+CREATE USER h4 PASSWORD 'h4' REPLICATION IN ROLE test_replica;
+
+SELECT pgwrh.add_shard_host('g1', 'h1', 'localhost', 5533);
+SELECT pgwrh.add_shard_host('g1', 'h2', 'localhost', 5534);
+SELECT pgwrh.add_shard_host('g1', 'h3', 'localhost', 5535);
+SELECT pgwrh.add_shard_host('g1', 'h4', 'localhost', 5536);
