fd62794298d0d64ec2cfdddbb6d7cf09737ea05f
Michal Kleczek <michal.kleczek@sap.com>
2025-01-29 09:39:13 +0100

    Added \echo to extension sql file

--- pgwrh--0.1.0.sql
+++ pgwrh--0.1.0.sql
@@ -14,6 +14,8 @@
 -- You should have received a copy of the GNU Affero General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+\echo Use "CREATE EXTENSION pgwrh CASCADE" to load this file. \quit
+
 GRANT USAGE ON SCHEMA @extschema@ TO PUBLIC;
 
 ALTER DEFAULT PRIVILEGES REVOKE EXECUTE ON ROUTINES FROM PUBLIC;
